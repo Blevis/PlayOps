2d8f1d09d8d529dd0c2cb9338b139ec1d15a2936
Blevis <blevis.allushi@gmail.com>
2025-10-31 15:36:26 +0100

    Concluded Game class (for the most part up until now), fulfilling sprint 1 requirements. Updated main to create objects from Game.

--- src/Game.java
+++ src/Game.java
@@ -1,72 +1,87 @@
 public class Game {
+    // PROPERTIES
     private int id;
     private String title;
     private int year;
-    private gameGenre genre;
-    private gamePlatform platform;
+    private GameGenre genre;
+    private GamePlatform platform;
     private String description;
+    private double pricePerDay;
 
-    public enum gameGenre{
-        ACTION, HORROR, SHOOTER;
+    // ENUMERATIONS
+    public enum GameGenre{
+        ACTION,
+        ADVENTURE,
+        ROLE_PLAYING,
+        SIMULATION,
+        STRATEGY,
+        SPORTS,
+        RACING,
+        FIGHTING,
+        HORROR,
+        PLATFORMER,
+        PUZZLE,
+        SANDBOX,
+        SHOOTER,
+        SURVIVAL,
+        STEALTH
     }
-    public enum gamePlatform{
-        PC, Xbox, PlayStation, NintendoSwitch;
-    }
+    public enum GamePlatform { PC, Xbox, PlayStation, NintendoSwitch }
 
-    public Game(int id, String title, int year, Game.gameGenre genre, Game.gamePlatform platform, String description){
+    // CONSTRUCTOR
+    public Game(int id, String title, int year, Game.GameGenre genre, GamePlatform platform, String description, double pricePerDay){
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platform = platform;
         this.description = description;
+        this.pricePerDay = pricePerDay;
     }
 
-    public int getId() {
-        return id;
+    // GET/SET METHODS
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getTitle() {return title;}
+
+    public void setTitle(String title) {this.title = title;}
+
+    public int getYear() {return year;}
+
+    public void setYear(int year) {this.year = year;}
+
+    public GameGenre getGenre() {return genre;}
+
+    public void setGenre(GameGenre genre) {this.genre = genre;}
+
+    public GamePlatform getPlatform() {return platform;}
+
+    public void setPlatform(GamePlatform platform) {this.platform = platform;}
+
+    public String getDescription() {return description;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public double getPricePerDay() {return pricePerDay;}
+
+
+    // OTHER METHODS
+    public double calculateRentalCost(int days) {
+        return pricePerDay * days;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    // TO STRING METHOD
+    @Override
+    public String toString() {
+        return "Game ID: " + id +
+                "\nTitle: " + title +
+                "\nYear: " + year +
+                "\nGenre: " + genre +
+                "\nPlatform: " + platform +
+                "\nDescription: " + description +
+                "\nPrice per day: "+ pricePerDay;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public gameGenre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(gameGenre genre) {
-        this.genre = genre;
-    }
-
-    public gamePlatform getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(gamePlatform platform) {
-        this.platform = platform;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Play Ops - Video Game rental management system");
+        System.out.println("Play Ops - Video Game rental management system\n");
+
+
+        // Testing game class
+        Game g1 = new Game(1, "Minecraft", 2009, Game.GameGenre.SANDBOX, Game.GamePlatform.PC, "Minecraft is a game made up of blocks, creatures, and community. You can survive the night or build a work of art – the choice is all yours.", 2.99);
+        Game g2 = new Game(2, "Doom Eternal", 2020, Game.GameGenre.SHOOTER, Game.GamePlatform.PC,"Doom Eternal is a first-person shooter where you play as the Slayer to stop demons from destroying Earth.", 4);
+        Game g3 = new Game(3, "Resident Evil Village", 2021, Game.GameGenre.HORROR, Game.GamePlatform.PlayStation, "Experience survival horror like never before in the eighth major installment in the storied Resident Evil franchise - Resident Evil Village", 4);
+
+        System.out.println(g1);
+        System.out.println(g2);
+        System.out.println(g3);
     }
 }
