23d13b25a168a6c9df8f11fc7d587bcc25849fca
KristiSeraj <serajkristi@gmail.com>
2025-11-03 09:50:51 +0100

    Store class created, added quantity to game class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Play Ops - Video Game rental management system\n");
 
         // Testing game + customer class
-        Game g1 = new Game("Minecraft", 2009, Game.GameGenre.SANDBOX, Game.GamePlatform.PC, "Minecraft is a game made up of blocks, creatures, and community. You can survive the night or build a work of art – the choice is all yours.", 2.99);
+        Game g1 = new Game("Minecraft", 2009, Game.GameGenre.SANDBOX, Game.GamePlatform.PC, "Minecraft is a game made up of blocks, creatures, and community. You can survive the night or build a work of art – the choice is all yours.", 1010.99);
         Game g2 = new Game("Doom Eternal", 2020, Game.GameGenre.SHOOTER, Game.GamePlatform.PC,"Doom Eternal is a first-person shooter where you play as the Slayer to stop demons from destroying Earth.", 4);
         Game g3 = new Game("Resident Evil Village", 2021, Game.GameGenre.HORROR, Game.GamePlatform.PlayStation, "Experience survival horror like never before in the eighth major installment in the storied Resident Evil franchise - Resident Evil Village", 4);
 
@@ -15,11 +15,18 @@ public class Main {
         System.out.println(g2);
         System.out.println(g3);
 
-        System.out.println("--------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------");
 
         System.out.println(c1);
         System.out.println(c2);
         System.out.println(c3);
 
+        Store s1 = new Store("Bobi Store");
+        s1.addGame(g1);
+        s1.addGame(g2);
+        s1.displayInventory();
+        s1.addCustomer(c1);
+        s1.addCustomer(c2);
+        s1.displayCustomers();
     }
 }
---
+++ src/Store.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+
+public class Store {
+    private String name;
+    private ArrayList<Game> inventory = new ArrayList<>();
+    private ArrayList<Customer> customers = new ArrayList<>();
+
+    public Store(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Game related methods
+    public void addGame(Game game) {
+        inventory.add(game);
+        game.setQuantity(game.getQuantity() + 1);
+        System.out.println(game.getTitle() + " has been added to the inventory.");
+    }
+
+    public void removeGame(String title) {
+        Game game = findGame(title);
+        if (game != null) {
+            inventory.remove(game);
+            System.out.println(title + " has been removed from the inventory.");
+        } else {
+            System.out.println(title + " not found in inventory.");
+        }
+    }
+
+    public Game findGame(String title) {
+        for (Game game : inventory) {
+            if (game.getTitle().equalsIgnoreCase(title)) {
+                return game;
+            }
+        }
+        return null;
+    }
+
+    public void displayInventory() {
+        System.out.println("Inventory: ");
+        if (inventory.isEmpty()) {
+            System.out.println(name + " has no inventory.");
+            return;
+        }
+        System.out.printf("| %-3s | %-25s | %-8s | %-10s |%n",
+                "ID", "Title", "Price/day", "Quantity");
+        System.out.println("-------------------------------------------------------------");
+        for (Game game : inventory) {
+            System.out.printf("| %-3d | %-25s | $%-8.2f | %-10d |%n", game.getId(), game.getTitle(), game.getPricePerDay(), game.getQuantity());}
+    }
+
+
+    // Customer related methods
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+        System.out.println("Customer " + customer.getName() + " " + customer.getLastName() + " has been added.");
+    }
+
+    public Customer findCustomer(String name) {
+        for (Customer customer : customers) {
+            if (customer.getName().equalsIgnoreCase(name)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public void removeCustomer(String name) {
+        Customer customer = findCustomer(name);
+        if (customer != null) {
+            customers.remove(customer);
+            System.out.println("Customer " + name  + " has been removed.");
+        } else {
+            System.out.println("Customer " + name + " not found.");
+        }
+    }
+
+    public void displayCustomers() {
+        System.out.println("\nCustomer List:");
+        if (customers.isEmpty()) {
+            System.out.println("No customers registered.");
+            return;
+        }
+
+        System.out.printf("| %-3s | %-20s | %-20s | %-20s |%n", "ID", "Name", "Email", "Address");
+        System.out.println("-----------------------------------------------------------------------------");
+        for (Customer customer : customers) {
+            String fullName = customer.getName() + " " + customer.getLastName();
+            System.out.printf("| %-3d | %-20s | %-20s | %-20s |%n",
+                    customer.getId(), fullName, customer.getEmail(), customer.getAddress());
+        }
+    }
+}
