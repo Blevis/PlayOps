5f99ba6eb43d29a7563bd40c8940ff7b6e12b70f
Blevis <blevis.allushi@gmail.com>
2025-12-13 00:00:43 +0100

    populated paymentmethod, cardpayment and cashpayment.

--- src/com/playops/payment/CashPayment.java
+++ src/com/playops/payment/CashPayment.java
@@ -1,4 +1,26 @@
 package com.playops.payment;
 
-public class CashPayment {
+import com.playops.exceptions.PaymentException;
+import com.playops.interfaces.PaymentMethod;
+
+public class CashPayment implements PaymentMethod{
+    private double cashProvided;
+
+    public CashPayment(double cashProvided){
+        if (cashProvided < 0)
+            throw new IllegalArgumentException("Cash provided cannot be negative");
+        this.cashProvided = cashProvided;
+    }
+
+    public double getCashProvided() {
+        return cashProvided;
+    }
+
+    @Override
+    public void pay(double amount) throws PaymentException{
+        if(cashProvided < amount){
+            throw new PaymentException("Not enough cash provided. Amount required: "+ amount);
+        }
+        System.out.println("Paid " + amount + " in cash. Change: " + (cashProvided - amount));
+    }
 }
